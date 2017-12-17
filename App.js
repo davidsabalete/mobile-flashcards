5e4b4e2b71c3d6aaba8b105d1977b653299010db
David Sabalete <dsabalete@gmail.com>
2017-12-17 14:47:05 +0100

    Basic structure with Stack and Tab navigation

--- App.js
+++ App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 import { Constants } from 'expo';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
-import { purple } from './utils/colors';
+import { purple, white } from './utils/colors';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import DeckListView from './components/DeckListView';
+import IndividualDeckView from './components/IndividualDeckView';
+import CreateDeckView from './components/CreateDeckView';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 const FlashcardStatusBar = ({ backgroundColor, ...props }) => (
-  <View>
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
     <StatusBar translucent backgroundColor={backgroundColor} {...props} />
   </View>
 );
+
+const Tab = TabNavigator({
+  DeckListView: {
+    screen: DeckListView,
+    navigationOptions: {
+      tabBarLabel: 'List',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  CreateDeck: {
+    screen: CreateDeckView,
+    navigationOptions: {
+      tabBarLabel: 'Create',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple: white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white: purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tab
+  },
+  Detail: {
+    screen: IndividualDeckView,
+    navigationOptions: {
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          <FlashcardStatusBar backgroundColor={purple} barStyle={light - content} />
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <FlashcardStatusBar backgroundColor={purple} barStyle='light-content' />
+          <Text>hola 1</Text>
+          <MainNavigator />
+          <Text>adios</Text>
         </View>
       </Provider>
     );
@@ -30,7 +85,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: white,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ components/CreateDeckView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class CreateDeckView extends Component {
+    render() {
+        return (
+            <View>
+                <Text>CreateDeckView</Text>
+            </View>
+        )
+    }
+}
+
+export default CreateDeckView
