cd04e6f64ed5d5ce3fbc342663f8a5fb3b29b3cb
David Sabalete <dsabalete@gmail.com>
2017-12-28 00:45:05 +0100

    Manage add_card action in reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,4 +1,4 @@
-import { RECEIVE_DECKS, ADD_DECK } from '../actions';
+import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions';
 
 function decks(state = {}, action) {
     switch (action.type) {
@@ -10,6 +10,14 @@ function decks(state = {}, action) {
                 [action.newDeck]: {
                     title: action.newDeck,
                     questions: []
+                }
+            }
+        case ADD_CARD:
+            return {
+                ...state,
+                [action.deck]: {
+                    ...state[action.deck],
+                    questions: state[action.deck].questions.concat(action.card)
                 }
             }
         default:
